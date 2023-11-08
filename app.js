dd4b333a8d9c23b87e520f59afd4856c4d4fa3d7
Jasmine Lai <jasmine.lai.03@gmail.com>
2023-11-08 15:12:43 -0500

    Finished up to 3.5 for a5

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from "cors";
 import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
 
 const app = express()
+app.use(cors());
 Hello(app)
 Lab5(app)
 app.listen(4000)

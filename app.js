8d0706b185f64b49ca32406492a8c5616731cdda
Jasmine Lai <jasmine.lai.03@gmail.com>
2023-11-16 15:43:59 -0500

    Finished up to EC with bug

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ import express from 'express';
 import cors from "cors";
 import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
 
 const app = express()
 app.use(cors());
+ModuleRoutes(app);
+CourseRoutes(app);
 app.use(express.json());
 Hello(app)
 Lab5(app)

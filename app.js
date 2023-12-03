9d1a4a48f3706ff2af2ff93202ae66ced4037384
Jasmine Lai <jasmine.lai.03@gmail.com>
2023-12-03 09:41:23 -0500

    fix mongoose connect error

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ import UserRoutes from "./users/routes.js";
 const DB_CONNECTION_STRING = 'mongodb+srv://jasmine-lai:cs4550kanbas@kanbas.s77gcwi.mongodb.net/?retryWrites=true&w=majority'
 const CONNECTION_STRING = DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 mongoose.connect(CONNECTION_STRING);
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express()
 app.use(cors({
     credentials: true,
